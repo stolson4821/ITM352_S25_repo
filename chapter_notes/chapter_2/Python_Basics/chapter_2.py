0bb135834c22f8cf6613b6c78cd7905f755aa872
stolson4821 <stolson@hawaii.edu>
2025-01-21 14:45:17 -1000

    PY function for text file

---
+++ chapter_notes/chapter_2/Python_Basics/chapter_2.py
@@ -0,0 +1,115 @@
+text = """
+Basic Elements of Python Code
+------------------------------
+
+Python Keywords:
+Python keywords are identifiers that are reserved words used by the interpreter for specific purposes. Variables and functions cannot use these reserved words.
+
+Common Python Keywords:
+False, Except, None, for, True, from, and, global, as, if, break, 
+import, continue, in, def, is, elif, not, else, or.
+
+Operators and Delimiters:
++ (Addition), - (Subtraction), * (Multiplication), / (Division),
+** (Exponentiation), < (Less than), > (Greater than),
+<= (Less than or equal to), >= (Greater than or equal to), 
+== (Equal to), != (Not equal to).
+
+Data Types:
+- Boolean (bool): Stores True/False values.
+- Integer (int): Stores whole numbers.
+- Float (float): Stores decimal numbers.
+- String (str): Stores text values.
+
+Comments:
+Begin with a # sign.
+
+Variable Assignment:
+Syntax: variable_name = value
+
+Mathematical Expressions:
++ (Addition), - (Subtraction), * (Multiplication), / (Division), 
+// (Integer Division), ** (Exponentiation).
+
+Python Built-in Exceptions:
+- IndexError: Accessing an out-of-range sequence index.
+- NameError: Using an undefined variable or function.
+- TypeError: Mismatch in data types.
+- ValueError: Argument is of correct type but has an improper value.
+- ZeroDivisionError: Dividing by zero.
+
+Python Built-in Functions:
+---------------------------
+
+A
+- abs(x): Returns the absolute value of a number.
+- all(iterable): Returns True if all elements are true or empty.
+- any(iterable): Returns True if any element is true.
+
+B
+- bin(x): Converts an integer to a binary string.
+- bool([x]): Converts a value to True or False.
+
+C
+- chr(i): Converts an integer to a Unicode character.
+- compile(source, filename, mode): Compiles source into a code object.
+
+D
+- dict(**kwargs): Creates a new dictionary.
+- dir([object]): Returns a list of attributes of the object.
+
+E
+- enumerate(iterable): Returns index-value pairs of an iterable.
+- eval(expression): Evaluates a Python expression.
+
+F
+- filter(function, iterable): Filters elements of an iterable.
+- float([x]): Converts a value to a floating-point number.
+
+G
+- getattr(object, name): Returns the named attribute of an object.
+
+H
+- help([object]): Displays help about an object.
+
+I
+- input([prompt]): Takes user input as a string.
+- int([x[, base]]): Converts a value to an integer.
+
+L
+- len(s): Returns the length of an object.
+- list([iterable]): Converts an iterable to a list.
+
+M
+- max(iterable): Returns the largest item in an iterable.
+- min(iterable): Returns the smallest item in an iterable.
+
+N
+- next(iterator): Returns the next item of an iterator.
+
+P
+- print(*objects): Prints objects to the screen.
+- property(fget=None): Returns a property attribute.
+
+R
+- range(start, stop[, step]): Returns a sequence of numbers.
+- round(number[, ndigits]): Rounds a number to a given precision.
+
+S
+- set([iterable]): Creates a set object.
+- str(object=''): Converts an object to a string.
+
+T
+- tuple([iterable]): Creates a tuple object.
+
+V
+- vars([object]): Returns the __dict__ attribute of an object.
+
+Z
+- zip(*iterables): Combines multiple iterables into tuples.
+
+These are the basics regarding Python coding. Refer to the Python documentation for further details.
+"""
+
+print(text)
+
