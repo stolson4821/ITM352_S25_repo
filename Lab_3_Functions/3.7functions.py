a95fb63aa35864ea28c1b0a69434e0b5022c8990
stolson4821 <stolson@hawaii.edu>
2025-02-09 20:52:33 -1000

    Completion of Lab 3 with extra credit

---
+++ Lab_3_Functions/3.7functions.py
@@ -0,0 +1,5 @@
+from HandyMath import arguments
+
+print(arguments(3, 5, min))
+print(arguments(3, 5, max))
+    
--- Lab_3_Functions/HandyMath.py
+++ Lab_3_Functions/HandyMath.py
@@ -14,3 +14,10 @@ def max(num1, num2):
 
 def min(num1, num2):
     return num1 if num1 <= num2 else num2
+
+#takes two numbers x,y and a function name as argumennts 
+#then returns a string "The function <function name>
+#x,y = <function applied to x,y>"
+
+def arguments(x, y, func):
+    return  f"The function {func.__name__}({x}, {y}) = {func(x, y)}"
