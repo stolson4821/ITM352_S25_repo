aeaab5e1aa712245b73e21ac393a4f603370b3a5
stolson4821 <stolson@hawaii.edu>
2025-04-10 12:35:52 -1000

    flask start

---
+++ HTML_FLASK_interface_Lab/1a_time_server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_daetime():
+    current_time = datetime.now().strftime('%y-%m-%d %H:%m:%S')
+    return f'Current Server Date and Time: {current_time}'
+
+if __name__ == '_main_':
+    app.run(debug=True)
